9bda75d984834d16be372247a3800b937c4997eb
Robert Jernberg <robert.jernberg@gmail.com>
2017-09-20 00:28:30 +0200

    Make SIMD neighbors implement IGetNeighbors
    Changes had been made in release. Adopt the SIMD neighbor solution
    to those changes.

--- TestBranchingStrategy/Program.cs
+++ TestBranchingStrategy/Program.cs
@@ -11,7 +11,7 @@ namespace TestBranchingStrategy
             Console.WriteLine("Hello, Conway's Game of Life!");
 
             var StartBoard = new HashSet<(int, int)>() { (0, 0), (1, 0), (2, 0), (0, 1), (1, 2) };
-            var NeighborFactory = new SimpleNeighbors();
+            IGetNeighbors NeighborFactory = new SimpleNeighbors();
 
             var CurrentSet = StartBoard;
             var SimpleGame = new SimpleGameOfLife();
@@ -30,6 +30,16 @@ namespace TestBranchingStrategy
             }
 
             Console.WriteLine("Second Game: " + FormatResylt(CurrentSet));
+
+
+            NeighborFactory = new SimdNeighbors();
+            CurrentSet = StartBoard;
+            for (int i = 0; i < 10; i++)
+            {
+                CurrentSet = SecondGame.Move(CurrentSet, NeighborFactory);
+            }
+
+            Console.WriteLine("Second Game SIMD: " + FormatResylt(CurrentSet));
 
             Console.ReadKey();
         }
